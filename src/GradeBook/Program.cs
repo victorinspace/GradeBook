c5eb7633d7ecf2becd90fbc96e200345bd50626a
Victor Velasquez <victorv24@gmail.com>
2019-07-30 10:00:07 -0700

    Inital Commit:
    Created GradeBook system for storing a list of grades, seperated into
    methods.
    Created Unit Tests for Adding and analyzing grades.

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Victor's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            
+            var stats = book.GetStatistics();
+            Console.WriteLine($"The highest grade is {stats.High}. The lowest grade is {stats.Low}. The average grade is {stats.Average:N1}.");
+        }
+    }
+}
